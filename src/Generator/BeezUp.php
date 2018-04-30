b768231d4d46d441ea26c7be7a9d6e13923a27f3
Max-Nils Brüschke <max.brueschke@plentymarkets.com>
2018-04-30 13:09:32 +0200

    laravel 5.5 update

--- src/Generator/BeezUp.php
+++ src/Generator/BeezUp.php
@@ -124,7 +124,7 @@ class BeezUp extends CSVPluginGenerator
 		$this->elasticExportPriceHelper = pluginApp(ElasticExportPriceHelper::class);
 
         $settings = $this->arrayHelper->buildMapFromObjectList($formatSettings, 'key', 'value');
-        $this->filtrationService = pluginApp(FiltrationService::class, [$settings, $filter]);
+		$this->filtrationService = pluginApp(FiltrationService::class, ['settings' => $settings, 'filterSettings' => $filter]);
         
         $this->setDelimiter(self::DELIMITER);
 
