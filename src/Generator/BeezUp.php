77107b18004d722fcfde3316b06828857bf6a72c
virlanremus <remus.virlan@plentymarkets.com>
2022-05-26 09:02:30 +0300

    Additional updates to ensure compatibility with PHP 8 Updated version + changelogs

--- src/Generator/BeezUp.php
+++ src/Generator/BeezUp.php
@@ -149,14 +149,14 @@ class BeezUp extends CSVPluginGenerator
 
                 $resultList = $elasticSearch->execute();
 
-				if(count($resultList['error']) > 0)
+				if(count($resultList['error'] ?? []) > 0)
 				{
 					$this->getLogger(__METHOD__)->error('ElasticExportBeezUp::log.ElasticSearchErrors', [
 						'Error message' => $resultList['error'],
 					]);
 				}
 
-                if(is_array($resultList['documents']) && count($resultList['documents']) > 0)
+                if(is_array($resultList['documents']) && count($resultList['documents'] ?? []) > 0)
                 {
                     foreach($resultList['documents'] as $variation)
                     {
